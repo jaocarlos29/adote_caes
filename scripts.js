d34856dd6fd848181d7d11a94dfe1cf4575f5a63
João Carlos da Silva <jaocarlos29@gmail>
2024-03-02 01:43:01 -0300

    correção do carrossel

--- scripts.js
+++ scripts.js
@@ -1,31 +1,41 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll('.carousel-image');
+// Declaração de variáveis globais
+let slideIndex = 0; // Índice do slide atual
+const slides = document.querySelectorAll('.carousel-image'); // Seleciona todos os elementos com a classe 'carousel-image'
 
+// Função para exibir o slide correspondente ao índice fornecido
 function showSlide(index) {
-  // Oculta todos os slides
-  slides.forEach(slide => {
-    slide.style.display = 'none';
-  });
+    // Oculta todos os slides
+    slides.forEach(slide => {
+        slide.style.display = 'none'; // Define o estilo de exibição como 'none' para ocultar o slide
+    });
 
-  // Garante que o índice do slide esteja dentro dos limites
-  if (index >= slides.length) {
-    slideIndex = 0;
-  } else if (index < 0) {
-    slideIndex = slides.length - 1;
-  }
+    // Garante que o índice do slide esteja dentro dos limites
+    if (index >= slides.length) {
+        slideIndex = 0; // Se o índice ultrapassar o número de slides, volta ao primeiro slide
+    } else if (index < 0) {
+        slideIndex = slides.length - 1; // Se o índice for menor que 0, volta ao último slide
+    }
 
-  // Exibe o slide atual
-//   slides[slideIndex].style.display = 'block';
+    // Exibe o slide atual
+    slides[slideIndex].style.display = 'block'; // Define o estilo de exibição como 'block' para mostrar o slide
 }
 
+// Função para avançar para o próximo slide
 function nextSlide() {
-  showSlide(slideIndex + 1);
+    slideIndex++; // Incrementa o índice do slide para avançar para o próximo slide
+    showSlide(slideIndex); // Chama a função showSlide() para exibir o próximo slide
 }
 
+// Função para retroceder para o slide anterior
 function prevSlide() {
-  showSlide(slideIndex - 1);
+    slideIndex--; // Decrementa o índice do slide para retroceder para o slide anterior
+    showSlide(slideIndex); // Chama a função showSlide() para exibir o slide anterior
 }
 
-setInterval(nextSlide, 3000); // Altera para o próximo slide a cada 3 segundos
+// Configura um intervalo de tempo para alterar automaticamente para o próximo slide a cada 5 segundos
+setInterval(nextSlide, 5000); // Chama a função nextSlide() a cada 5000 milissegundos (5 segundos)
 
-showSlide(slideIndex);
+// Evento 'onload' que é acionado quando a página é carregada
+window.onload = function() {
+    showSlide(slideIndex); // Chama a função showSlide() para exibir o slide atual quando a página é carregada
+};
